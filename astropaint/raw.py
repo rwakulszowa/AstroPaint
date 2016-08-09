2587a13862cd45edf783ec6cebe17fb86aefed42
rwakulszowa <rwakulszowa1@gmail.com>
2016-08-09 20:14:59 +0200

    Implemented own downloader

--- astropaint/raw.py
+++ astropaint/raw.py
@@ -1,4 +1,8 @@
+import os
+import urllib.parse
+
 import numpy as np
+import requests
 
 from astropy.io import fits
 
@@ -8,12 +12,19 @@ class Raw(object):
 
     def __init__(self, urls, kind):
         self.urls = urls
+        self.encoded_urls = [urllib.parse.quote_plus(u) for u in self.urls]
         self.kind = kind
-        self.data = np.dstack([self._normalize(self._read(u)) for u in self.urls])
+        self.data = np.dstack([self._normalize(self._read(u, "./temp/{}".format(p)))
+                               for u, p in zip(self.urls, self.encoded_urls)])
 
-    @staticmethod
-    def _read(url):
-        data = fits.getdata(url, cache=True, show_progress=False)
+    @classmethod
+    def _read(cls, url, path):
+        try:
+            data = fits.getdata(path)
+        except FileNotFoundError:
+            with open(path, "wb") as f:
+                f.write(requests.get(url).content)
+            data = fits.getdata(path)
         return data
 
     @staticmethod
