b6f7a82108babc62b83cb9f77cc6db4e280822c4
rwakulszowa <rwakulszowa1@gmail.com>
2016-09-01 21:45:27 +0200

    Implemented clear_images

--- manage.py
+++ manage.py
@@ -14,18 +14,22 @@ parser.add_argument(dest="command", help="One of: cleardb, evaluate")
 DBS = ["analysis", "classification", "processing"]
 SERVER = couchdb.client.Server(config.DB_ADDRESS)
 
-def cleardb():
+def clear_db():
     print("Clearing databases {} in {}".format(DBS, config.DB_ADDRESS))
     for db in DBS:
         db = SERVER[db]
         docs = [db[id] for id in db if not id.startswith('_')]
         db.purge(docs)
 
+def clear_images():
+    print("Clearing images in {}".format(config.STORAGE_DIR))
+    for i in _get_images():
+        os.remove(os.path.join(config.STORAGE_DIR, i))
+
 def evaluate():
     db = SERVER["processing"]
-    images = [p.split('.')[0] for p in os.listdir(config.STORAGE_DIR)
-              if any(p.endswith(e) for e in [".jpg", ".jpeg", ".png", ".bmp"])]
-    for id in images:
+    ids = [i.split('.')[0] for i in _get_images()]
+    for id in ids:
         processed = db[id]
         if len(processed["evaluation"]) == 0:
             im = PIL.Image.open(config.STORAGE_DIR + id + ".png")
@@ -34,10 +38,16 @@ def evaluate():
             processed["evaluation"].append(int(e))
             db[id] = processed
 
+def _get_images():
+    return [p for p in os.listdir(config.STORAGE_DIR)
+              if any(p.endswith(e) for e in [".jpg", ".jpeg", ".png", ".bmp"])]
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
     commands = {
-        "cleardb": cleardb,
+        "clear_db": clear_db,
+        "clear_images": clear_images,
         "evaluate": evaluate,
     }
     commands[args.command]()
